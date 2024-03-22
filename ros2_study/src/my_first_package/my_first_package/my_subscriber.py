b838b69c9e7814ce7752fa2b8e4f667d0b9ae44f
MinJaehyun <krism01@naver.com>
2024-03-22 17:01:00 +0900

    주석제거

--- ros2_study/src/my_first_package/my_first_package/my_subscriber.py
+++ ros2_study/src/my_first_package/my_first_package/my_subscriber.py
@@ -6,11 +6,11 @@ from turtlesim.msg import Pose
 class TurtlesimSubscriber(Node):                    # Node를 상속한다
   def __init__(self):
     super().__init__('turtlesim_subscriber')        # 던드 init 사용 시, 노드 클래스는 초기화하기 위해 노드 이름 설정해야 한다. 
-    self.subscription = self.create_subscription(Pose, 'turtle1/pose', self.callback, 10)
+    self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.callback, 10)
     self.subscription 
 
   def callback(self, msg):
-    print("X: ", msg.xm, ", Y: ", msg.y)
+    print("X: ", msg.x, ", Y: ", msg.y)
 
 
 def main(args=None):
